3d5fb27a44ff865453782bdcbcb358dc8d07558a
mango <ding.luo@dingcom.com>
2021-01-21 16:57:24 +0800

    cut_line.sh，按start_line和end_line切文件，合并了head_file.sh和tail_file.sh的功能

---
+++ cut_line.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+if [ $# -ne 3 ]
+then
+    echo "input error, help info"
+    echo "$0 file start_line end_line"
+    echo "start_line=1 means the first line"
+    echo "end_line=0 means the last line"
+    echo "eg: $0 01-01-log.txt 20 100"
+    echo ""
+    exit 0
+fi
+
+src_file=$1
+start_line=$2
+end_line=$3
+dest_file="$src_file.log"
+
+total_line=$(wc -l $src_file |cut -d' ' -f1)
+if [ $end_line -gt $total_line ] || [ $end_line -le 0 ]
+then
+    end_line=$total_line
+fi
+echo "end_line=$total_line"
+
+if [ $start_line -gt $end_line ]
+then
+    echo "error, start_line=$start_line >= end_line=$end_line"
+    exit 0
+fi
+
+if [ $start_line -eq 1 ]
+then
+    head -n $end_line $src_file > $dest_file
+elif [ $end_line -eq $total_line ]
+then
+    tail -n $((total_line - start_line + 1)) $src_file > $dest_file
+else
+    bak_file="$src_file.bak"
+    head -n $end_line $src_file > $bak_file
+    total_line=$end_line
+    tail -n $((total_line - start_line + 1)) $bak_file > $dest_file
+    rm -f $bak_file
+fi
+
+echo "ok"
